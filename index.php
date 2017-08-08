8655cc8f76c1dc9e6f982e2c02c86e7c30c1b6d2
Diego Najar <dignajar@gmail.com>
2017-08-08 19:45:20 +0200

    minor fixes

--- index.php
+++ index.php
@@ -3,32 +3,32 @@ $version = array(
 	'stable'=>array(
 		'version'=>'1.6.2',
 		'codeName'=>'',
-		'build'=>'20170606',
-		'releaseDate'=>'2017-06-06',
-		'downloadLink'=>'',
+		'build'=>'20170504',
+		'releaseDate'=>'2017-05-04',
+		'downloadLink'=>'https://s3.amazonaws.com/bludit-s3/bludit-builds/bludit-v1.6.2.zip',
 		'downloadMirrorLink'=>'',
-		'changelogLink'=>'',
-		'github'=>''
+		'changelogLink'=>'https://github.com/dignajar/bludit/releases/tag/1.6.2',
+		'github'=>'https://github.com/dignajar/bludit/releases/tag/1.6.2'
 	),
 	'beta'=>array(
-		'version'=>'2.0',
+		'version'=>'',
 		'codeName'=>'',
-		'build'=>'20170606',
-		'releaseDate'=>'2017-06-06',
+		'build'=>'',
+		'releaseDate'=>'',
 		'downloadLink'=>'',
 		'downloadMirrorLink'=>'',
 		'changelogLink'=>'',
 		'github'=>''
 	),
 	'nigth'=>array(
-		'version'=>'2.0',
+		'version'=>'2.0 Alpha 3',
 		'codeName'=>'',
-		'build'=>'20170606',
-		'releaseDate'=>'2017-06-06',
+		'build'=>'',
+		'releaseDate'=>'',
 		'downloadLink'=>'',
 		'downloadMirrorLink'=>'',
-		'changelogLink'=>'',
-		'github'=>''
+		'changelogLink'=>'https://github.com/dignajar/bludit/releases/tag/2.0-alpha3',
+		'github'=>'https://github.com/dignajar/bludit/releases/tag/2.0-alpha3'
 	)
 );
 if( !defined('BLUDIT')) {
