313f4df6ddcbcc5bd488daa9d807998e5ec3064e
Diego Najar <dignajar@gmail.com>
2018-01-23 17:31:52 +0100

    Bludit v2.2.1

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 $version = array(
 	'stable'=>array(
-		'version'=>'2.1.1',
-		'codeName'=>'Rick',
-		'build'=>'20180102',
-		'releaseDate'=>'2018-01-02',
-		'downloadLink'=>'https://df6m0u2ovo2fu.cloudfront.net/builds/bludit-2-1-1.zip',
+		'version'=>'2.2.1',
+		'codeName'=>'Pepper',
+		'build'=>'20180123',
+		'releaseDate'=>'2018-01-23',
+		'downloadLink'=>'https://df6m0u2ovo2fu.cloudfront.net/builds/bludit-2-2-1.zip',
 		'downloadMirrorLink'=>'',
-		'changelogLink'=>'https://github.com/bludit/bludit/releases/tag/2.1.1',
-		'github'=>'https://github.com/bludit/bludit/releases/tag/2.1.1',
-		'blog'=>'https://blog.bludit.com/bludit-2-1-1'
+		'changelogLink'=>'https://github.com/bludit/bludit/releases/tag/2.2.1',
+		'github'=>'https://github.com/bludit/bludit/releases/tag/2.2.1',
+		'blog'=>'https://blog.bludit.com/bludit-2-2-1'
 	),
 	'beta'=>array(
 		'version'=>'',
