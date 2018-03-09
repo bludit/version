679d5539333d042ad3eefa2615e63843e5172019
Diego Najar <dignajar@gmail.com>
2018-03-09 20:14:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 $version = array(
 	'stable'=>array(
-		'version'=>'2.3',
+		'version'=>'2.3.1',
 		'codeName'=>'1-Commit',
-		'build'=>'20180308',
-		'releaseDate'=>'2018-03-08',
-		'downloadLink'=>'https://df6m0u2ovo2fu.cloudfront.net/builds/bludit-2-3.zip',
+		'build'=>'20180309',
+		'releaseDate'=>'2018-03-09',
+		'downloadLink'=>'https://df6m0u2ovo2fu.cloudfront.net/builds/bludit-2-3-1.zip',
 		'downloadMirrorLink'=>'',
-		'changelogLink'=>'https://github.com/bludit/bludit/releases/tag/2.3',
-		'github'=>'https://github.com/bludit/bludit/releases/tag/2.3',
-		'blog'=>'https://blog.bludit.com/bludit-2-3'
+		'changelogLink'=>'https://github.com/bludit/bludit/releases/tag/2.3.1',
+		'github'=>'https://github.com/bludit/bludit/releases/tag/2.3.1',
+		'blog'=>'https://blog.bludit.com/bludit-2-3-1'
 	),
 	'beta'=>array(
 		'version'=>'',
