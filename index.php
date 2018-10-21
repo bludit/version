6542619716a000a651d9d1b42096739fb90aee3b
Diego Najar <dignajar@gmail.com>
2018-10-21 17:53:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 $version = array(
 	'stable'=>array(
-		'version'=>'3.1.0',
-		'codeName'=>'Malts',
-		'build'=>'20181008',
-		'releaseDate'=>'2018-10-08',
-		'downloadLink'=>'https://bludit.s3.nl-ams.scw.cloud/bludit-3-1-0.zip',
+		'version'=>'3.2.0',
+		'codeName'=>'Yeast',
+		'build'=>'20181020',
+		'releaseDate'=>'2018-10-20',
+		'downloadLink'=>'https://bludit.s3.nl-ams.scw.cloud/bludit-3-2-0.zip',
 		'downloadMirrorLink'=>'',
-		'changelogLink'=>'https://github.com/bludit/bludit/releases/tag/3.1.0',
-		'github'=>'https://github.com/bludit/bludit/releases/tag/3.1.0',
-		'blog'=>'https://blog.bludit.com/bludit-3-1-0'
+		'changelogLink'=>'https://github.com/bludit/bludit/releases/tag/3.2.0',
+		'github'=>'https://github.com/bludit/bludit/releases/tag/3.2.0',
+		'blog'=>'https://blog.bludit.com/bludit-3-2-0'
 	),
 	'beta'=>array(
 		'version'=>'',
