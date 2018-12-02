9f200c9f73e29865a82c84cd6723d5dfd651abdc
Diego Najar <dignajar@gmail.com>
2018-12-02 12:41:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 $version = array(
 	'stable'=>array(
-		'version'=>'3.4.0',
-		'codeName'=>'Beer',
-		'build'=>'20181110',
-		'releaseDate'=>'2018-11-10',
-		'downloadLink'=>'https://bludit.s3.nl-ams.scw.cloud/bludit-3-4-0.zip',
+		'version'=>'3.5.0',
+		'codeName'=>'IPA',
+		'build'=>'20181201',
+		'releaseDate'=>'2018-12-01',
+		'downloadLink'=>'https://bludit.s3.nl-ams.scw.cloud/bludit-3-5-0.zip',
 		'downloadMirrorLink'=>'',
-		'changelogLink'=>'https://github.com/bludit/bludit/releases/tag/3.4.0',
-		'github'=>'https://github.com/bludit/bludit/releases/tag/3.4.0',
-		'blog'=>'https://blog.bludit.com/bludit-3-4-0'
+		'changelogLink'=>'https://github.com/bludit/bludit/releases/tag/3.5.0',
+		'github'=>'https://github.com/bludit/bludit/releases/tag/3.5.0',
+		'blog'=>'https://blog.bludit.com/bludit-3-5-0'
 	),
 	'beta'=>array(
 		'version'=>'',
